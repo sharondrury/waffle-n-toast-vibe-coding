3c34302d29e0da75a5672f512607f0a686ebc971
Sharon Drury <98319755+sharondrury@users.noreply.github.com>
2026-03-12 16:14:29 +0000

    changed routing imports

--- vibe-coding/src/main.jsx
+++ vibe-coding/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.scss'
 import App from './App.jsx'
 
@@ -10,8 +10,8 @@ import "primeicons/primeicons.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
